f3bfbdacde4888ecb59d4e9063e040354537ac64
arvid <arvid@bfe20620-3651-4789-ab27-ac0491693af1>
2008-10-23 15:14:59 +0000

    Removed order_items from list
    git-svn-id: http://winston.svn.beanstalkapp.com/porthos/trunk@54 bfe20620-3651-4789-ab27-ac0491693af1

--- db/migrate/115_remove_unicef_tables.rb
+++ db/migrate/115_remove_unicef_tables.rb
@@ -2,7 +2,7 @@ class RemoveUnicefTables < ActiveRecord::Migration
   def self.up
     raise "This migration will remove UNICEF specfic tables" if RAILS_ROOT.include?('unicef')
     [ :activity_groups, :carts, :credit_check_responses, :discounteds,
-      :discounts,:global_parent_give_away_recipients, :order_items, :orders, 
+      :discounts,:global_parent_give_away_recipients, :orders, 
       :print_settings, :product_categories, :product_categorizations, :product_images, 
       :products, :sample_product_order_items, :shipments, :shops, :vg_cart_items, :vg_carts, 
       :vg_categories, :vg_categorizations, :virtual_gifts
