7ed39104b2ee61bf837717b9ffb2f840b297f02e
arvida <arvid@winstondesign.se>
2010-01-13 14:43:29 +0100

    Made page tip send several mails instead of one

--- app/models/page_tip_mailer.rb
+++ app/models/page_tip_mailer.rb
@@ -1,8 +1,8 @@
 class PageTipMailer < ActionMailer::Base
-  def tip(page_tip)
+  def tip(page_tip, recipient)
     @subject    = "#{page_tip.name} vill tipsa dig om en sida på UNICEF Sveriges webbplats"
     @body       = {:page_tip => page_tip}
-    @recipients = page_tip.recipients.split(' ')
+    @recipients = recipient
     @from       = 'no.reply@unicef.se'
   end
 end
