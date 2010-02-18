8485f6352cc45e91b6364ba9caea617990476c45
arvida <arvid@winstondesign.se>
2010-02-18 17:56:55 +0100

    Fixed redirects

--- app/metal/redirects.rb
+++ app/metal/redirects.rb
@@ -3,8 +3,15 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 
 class Redirects
   def self.call(env)
-    redirect = Redirect.connection.select_all("SELECT * FROM redirects WHERE path = '#{env["PATH_INFO"]}'").first
+    redirect = Redirect.connection.select_all("SELECT * FROM redirects WHERE path = '#{env['PATH_INFO']}'").first
     if redirect && (redirect_path = redirect['target'])
+      if not env['QUERY_STRING'].blank?
+        if redirect_path.include?('?')
+          redirect_path.gsub!('?', "?#{env['QUERY_STRING']}&")
+        else
+          redirect_path << "?#{env['QUERY_STRING']}"
+        end
+      end
       [302, {'Content-Type' => 'text/html', 'Location' => redirect_path}, ['You are being redirected.']]
     else
       [404, {'Content-Type' => 'text/html'}, ['Not Found']]
