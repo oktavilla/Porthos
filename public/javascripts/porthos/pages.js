feae85448e27168a4a3d97c393d33c85c7c55646
Joel Junström <joel@winstonjoel.lan>
2010-02-26 20:30:59 +0100

    New sorting for page contents

--- public/javascripts/porthos/pages.js
+++ public/javascripts/porthos/pages.js
@@ -16,42 +16,12 @@
           element  : div.select('ul.contents')[0]
         }
       });
-      this.contents = this.columns_container.select('ul.contents li.content').collect(function(element) {
-//        return Porthos.Pages.Contents.create(element);
-      }.bind(this));
-//      this.contents.invoke('observeLinks');
-      this.sorting = false;
-      this.observeSortContents();
-      // New contents
-      
-      this.initializeDialogInvokers();
+      this.setupSorting();
     },
     
-    initializeDialogInvokers: function() {
-      this.columns.each(function(column) {
-        column.container.select('a.add').invoke('observe', 'click', function(event) {
-          element = event.element();
-          if (!element.hasClassName('active')) {
-            element.addClassName('active');
-          } else {
-            element.removeClassName('active');
-          }
-          column.container.select('div.sub_controls').invoke('toggle');
-          event.stop();
-        }.bind(this));
-      }.bind(this));
-    },
-    
-    observeSortContents: function() {
-      $$('a.sort').invoke('observe', 'click', this.setupSorting.bindAsEventListener(this));
-    },
-    
-    setupSorting: function(event) {
-      if (this.sorting) { return false; }
-      else { this.sorting = true; }
+    setupSorting: function() {
       this.sortables = this.columns_container.select('ul.sortable');
       this.teaserCollections = this.columns_container.select('ul.teasers');
-      this.columns_container.addClassName('sorting');
       this.sortables.reverse().each(function(sortable) {
         sortable.isColumn = sortable.hasClassName('contents');
         if (!sortable.isColumn) {
@@ -74,25 +44,13 @@
           scroll      : window,
           handle      : 'draghandle',
           only        : 'sortable',
-          constraint  : false
+          constraint  : false,
+          onUpdate    : this.sort.bindAsEventListener(this)
         });
       }.bind(this));
-      this.columns.each(function(column) {
-        form = $form(
-          { 'action': Routing.sort_admin_contents_path() },
-          $input({ 'type': 'submit', 'value': 'Spara sortering', 'class': 'button' }),
-          ' eller ',
-          $a({ 'href': '' }, 'avbryt')
-        );
-        form.observe('submit', this.sort.bindAsEventListener(this));
-        column.element.parentNode.insertBefore($div({ 'class': 'confirm' }, form), column.element);
-        $(this.columns_container).select('span.controls, div.sub_controls').invoke('hide');
-      }.bind(this));
-      event.stop();
     },
     
     sort: function(event) {
-      event.element().blur();
       this.sortables.each(function(sortable) {
         var params = '';
         if (sortable.isColumn) {
@@ -107,41 +65,6 @@
           }) + params
         });
       }.bind(this));
-      this.teardownSorting();
-      event.stop();
-    },
-    
-    teardownSorting: function(event) {
-      this.sortables.each(function(sortable) {
-        Sortable.destroy(sortable);
-      });
-      $(this.columns_container).select('div.confirm').invoke('remove');
-      $(this.columns_container).select('span.controls').invoke('show');
-      this.columns_container.removeClassName('sorting');
-      var publish_link = $a({
-        'href' : Routing.publish_admin_page_path({ 'id' : this.id })
-      }, 'Publicera ändringarna');
-      publish_link.observe('click', function(event) {
-        event.stop();
-        var link = event.element();
-        var f = $form({
-          'action' : link.href,
-          'method' : 'post',
-          'style'  : 'display:none;'
-        }, $input({
-          'type'  : 'hidden',
-          'name'  : '_method',
-          'value' : 'put'
-        }));
-        link.parentNode.appendChild(f);
-        f.submit();
-      });
-      if ($$('p.notice').size() == 0) {
-        this.columns_container.insert({
-          'top' : $p({ 'class' : 'notice' }, publish_link)
-        });
-      }
-      this.sorting = false;
     }
   });
   
