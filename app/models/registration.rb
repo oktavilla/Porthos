d7a5deb59f4bceda349848256fd885c24d5b053a
joeljunstrom <joel@winstondesign.se>
2010-02-28 23:49:11 +0100

    cognetated the registration fields to person and organization

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,62 +1,3 @@
-# == Schema Information
-# Schema version: 76
-#
-# Table name: registrations
-#
-#  id                       :integer(11)   not null, primary key
-#  type                     :string(255)   
-#  first_name               :string(255)   
-#  last_name                :string(255)   
-#  customer_number          :string(255)   
-#  age                      :integer(11)   
-#  email                    :string(255)   
-#  phone                    :string(255)   
-#  cell_phone               :string(255)   
-#  organization             :string(255)   
-#  department               :string(255)   
-#  organization_number      :string(255)   
-#  address                  :string(255)   
-#  post_code                :string(255)   
-#  locality                 :string(255)   
-#  country                  :string(255)   
-#  shipping_address         :string(255)   
-#  shipping_post_code       :string(255)   
-#  shipping_locality        :string(255)   
-#  shipping_country         :string(255)   
-#  contact_approval         :boolean(1)    
-#  total_sum                :integer(11)   default(0), not null
-#  total_vat                :integer(11)   default(0), not null
-#  total_items              :integer(11)   
-#  payment_id               :integer(11)   
-#  payment_type             :string(255)   
-#  payment_transaction_id   :string(255)   
-#  payment_status           :string(255)   
-#  payment_response_message :string(255)   
-#  public_id                :string(255)   
-#  user_id                  :integer(11)   
-#  shop_id                  :integer(11)   
-#  dispatch_id              :string(255)   
-#  dispatch_status          :string(255)   
-#  shipment_id              :string(255)   
-#  shipment_type            :string(255)   
-#  message                  :string(255)   
-#  node_id                  :integer(11)   
-#  created_at               :datetime      
-#  updated_at               :datetime      
-#  registration_form_id     :integer(11)   
-#  donation                 :integer(11)   default(0), not null
-#  shipment_price           :integer(11)   default(0), not null
-#  activity_group_id        :integer(11)   
-#  school                   :string(255)   
-#  school_class             :string(255)   
-#  number_of_persons        :integer(11)   
-#  uri                      :string(255)   
-#  recipients               :string(255)   
-#  target_code              :string(255)   
-#  shipment_vat             :integer(11)   default(0)
-#  shipping_organisation    :string(255)   
-#
-
 class Registration < ActiveRecord::Base
 
   def sale?; false; end
@@ -118,10 +59,32 @@ class Registration < ActiveRecord::Base
     donation.to_money rescue 0.to_money
   }
   
-  is_indexed :fields => ['id', 'type', 'first_name', 'last_name', 'customer_number', 'email', 'organization', 'address', 
-                         'locality', 'shipping_first_name', 'shipping_last_name','shipping_organisation', 'shipping_address', 
-                         'shipping_locality', 'public_id', 'message', 'school', 'payment_transaction_id', 'dispatch_id'],
-                         :concatenate => [{:association_name => 'conversion', :field => 'measure_point_id', :as => 'campaign_measure_point_id'}]
+  is_indexed({
+    :fields => ['public_id', 'payment_transaction_id', 'dispatch_id'],
+    :concatenate => [
+      {
+        :fields => [
+          'first_name', 'last_name', 'civic_number', 'customer_number',
+          'email', 'phone', 'cell_phone',
+          'address', 'co_address', 'post_code', 'locality', 'country',
+          'shipping_first_name', 'shipping_last_name',
+          'shipping_address', 'shipping_co_address', 'shipping_post_code', 'shipping_locality', 'shipping_country'
+        ],
+        :as => 'person'
+      }, {
+        :fields => [
+          'organization',
+          'department',
+          'organization_number',
+          'school'
+        ],
+        :as => 'organization'
+      }, {
+      :association_name => 'conversion',
+      :field => 'measure_point_id',
+      :as => 'campaign_measure_point_id'
+    }]
+  })
                          
   
   HUMANIZED_STATUSES = {
