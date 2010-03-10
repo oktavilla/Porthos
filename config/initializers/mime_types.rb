f9b959a789e1c2a5a2e0fec30855ab09c48d4cb9
joeljunstrom <joel@winstondesign.se>
2010-03-10 15:53:51 +0100

    Added jpeg mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,8 +2,9 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register "application/x-mobile", :mobile
 # Image formats
-Mime::Type.register "image/jpg", :jpg
-Mime::Type.register "image/gif", :gif
+Mime::Type.register "image/jpg",  :jpg
+Mime::Type.register "image/jpeg", :jpeg
+Mime::Type.register "image/gif",  :gif
 Mime::Type.register "image/png", :png
 # Movie formats
 Mime::Type.register "video/quicktime", :mov
