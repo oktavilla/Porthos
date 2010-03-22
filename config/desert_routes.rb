72b0176657062a9d01b7309a76d91b424971cca9
arvida <arvid@winstondesign.se>
2010-03-22 11:08:21 +0100

    Removed typo

--- config/desert_routes.rb
+++ config/desert_routes.rb
@@ -92,7 +92,7 @@ ActionController::Routing::Routes.draw do |map|
     end
 
     admin.registrations         '/registrations',               :controller => 'registrations', :action => 'index'
-    admin.comment_registration  '/registrations/comment',       :controller => 'registrations', :action => 'comment'¯
+    admin.comment_registration  '/registrations/comment',       :controller => 'registrations', :action => 'comment'
     admin.invalid_registrations_by_type '/registrations/:type/invalid', :controller => 'registrations', :action => 'invalid'
     admin.registration          '/registrations/:type/:id',     :controller => 'registrations', :action => 'show'
     admin.export_registration   '/registrations/export/:type.:format',  :controller => 'registrations', :action => 'period'
